996091af54feb1643866b4c066f45891f7ad2f96
Gowtham Behara <gothsiddu@gmail.com>
2019-01-29 14:40:09 +0530

    Define func to parse args

--- index.py
+++ index.py
@@ -1,8 +1,27 @@
-import logging
+import logging, argparse
+
+
+def parse_args():
+    logger.info("Started executing parse_args()")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source-repo', help="Repository from which artifact should be copied/moved", required=True)
+    parser.add_argument('-c', '--component', help="Name of the component which has to be copied/moved")
+    parser.add_argument('--component-version', help="Version of the component which has to be copied/moved", required=True)
+    parser.add_argument('--host', help="Host address of nexus repository", default="http://192.168.113.192:15921")
+    parser.add_argument('-u', '--username', help="Username of the nexus repository admin", default="admin")
+    parser.add_argument('-p', '--password', help="Password of the nexus repository admin", default="admin123")
+
+    logger.info("parse_args function execution finished")
+    return vars(parser.parse_args())
 
 
 def main():
     logger.info("Started executing main function")
+
+    args = parse_args()
+    logger.debug("Arguments parsed are: " + str(args))
+
     logger.info("Main function execution finished.")
 
 
